ce27536e2d67a5b9979c97f54e24c41cce7ceabc
CHARAN G <charang2003@gmail.com>
2024-05-11 12:20:08 +0530

    made some changes

--- script.js
+++ script.js
@@ -2,6 +2,10 @@
 const darkModeToggle = document.getElementById('darkModeToggle');
 const body = document.body;
 const isDarkMode = localStorage.getItem('darkMode') === 'enabled';
+if (isDarkMode){
+    body.classList.add('dark-mode');
+    darkModeToggle.checked = true;
+}
 darkModeToggle.addEventListener('change', () => {
     if (darkModeToggle.checked){
         body.classList.add('dark-mode');
